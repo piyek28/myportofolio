e0deee679a4b2e782c0f8f7b72be8b6b2f20bd43
piyek28 <140377865+piyek28@users.noreply.github.com>
2023-08-10 18:52:02 +0700

    Update 4.3.0 Content

---
+++ resources/views/partial/content.blade.php
@@ -0,0 +1,89 @@
+<div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+        <div class="col-lg-4">
+            <img class="bd-placeholder-img rounded-circle"
+                 width="140" height="140" src="{{ asset('img/bg1.jpg') }}"
+                 alt="Background">
+            <h2 class="fw-normal">Heading</h2>
+            <p>Some representative placeholder content for the three columns of text below the carousel. This is the
+                first column.</p>
+            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="bd-placeholder-img rounded-circle"
+                 width="140" height="140" src="{{ asset('img/bg2.jpg') }}"
+                 alt="Background">
+            <h2 class="fw-normal">Heading</h2>
+            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second
+                column.</p>
+            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="bd-placeholder-img rounded-circle"
+                 width="140" height="140" src="{{ asset('img/bg3.jpg') }}"
+                 alt="Background">
+            <h2 class="fw-normal">Heading</h2>
+            <p>And lastly this, the third column of representative placeholder content.</p>
+            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+
+
+    <!-- START THE FEATURETTES -->
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-7">
+            <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span
+                    class="text-body-secondary">It’ll blow your mind.</span></h2>
+            <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting
+                prose here.</p>
+        </div>
+        <div class="col-md-5">
+            <img class="bd-placeholder-img"
+                 width="500" height="500" src="{{ asset('img/bg3.jpg') }}"
+                 alt="Background">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span
+                    class="text-body-secondary">See for yourself.</span></h2>
+            <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how
+                this layout would work with some actual real-world content in place.</p>
+        </div>
+        <div class="col-md-5 order-md-1">
+            <img class="bd-placeholder-img"
+                 width="500" height="500" src="{{ asset('img/bg2.jpg') }}"
+                 alt="Background">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-7">
+            <h2 class="featurette-heading fw-normal lh-1">And lastly, this one. <span class="text-body-secondary">Checkmate.</span>
+            </h2>
+            <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really
+                intended to be actually read, simply here to give you a better view of what this would look like
+                with some actual content. Your content.</p>
+        </div>
+        <div class="col-md-5">
+            <img class="bd-placeholder-img"
+                 width="500" height="500" src="{{ asset('img/bg1.jpg') }}"
+                 alt="Background">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <!-- /END THE FEATURETTES -->
+
+</div><!-- /.container -->
